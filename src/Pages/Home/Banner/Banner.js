515aa26ad77785dcdcc352551442f22b2c460e2c
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-12 00:57:05 +0600

    banner done.

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png';
 
 const Banner = () => {
     return (
-        <div>
-                
-        </div>
+
+        <div className="hero min-h-[600px]" style={{
+            backgroundImage: `url('/bg.png')`
+        }}>
+            <div className="hero-content flex flex-col  justify-center items-center lg:flex-row-reverse">
+                <img src={chair} className="lg:w-1/2 my-auto rounded-lg shadow-2xl h-[355px]" alt='' />
+                <div className='text-center lg:text-left mt-8 lg:mt-0'>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+                </div>
+            </div>
+        </div >
+
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // 'hero-pattern': "url('bg.png')",
+      }
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          "primary": "#0FCFEC",
+          "secondary": "#19D3AE",
+          "accent": "#3A4256",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
 }
