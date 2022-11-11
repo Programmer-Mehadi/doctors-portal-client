dacb38e445e1c4339071a85b9ad0c04877ca6cde
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-12 02:33:51 +0600

    make appointment done.

---
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import appointment from '../../../assets/images/appointment.png';
+import doctor from '../../../assets/images/doctor.png';
+const MakeAppointment = () => {
+    return (
+        <div className='py-6 md:py-32'>
+             <div className='mt-6 lg:h-[440px] ' style={{
+            backgroundImage: `url(${appointment})`
+        }}>
+            <div className="hero ">
+                <div className="hero-content grid grid-cols-1 md:grid-cols-2  lg:px-10 ">
+                    <img src={doctor} className="h-[600px] md:-mt-[175px]  rounded-lg  hidden md:flex  " />
+                    <div className='p-8 md:p-0'>
+                        <h2 className="text-2xl text-secondary font-bold">Appointment</h2>
+                        <h1 className="text-4xl font-bold text-white">Make an appointment Today</h1>
+                        <p className="py-6 text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
+    );
+};
+
+export default MakeAppointment;
--- src/Pages/Home/TreatmentBanner/TreatmentBanner.js
+++ src/Pages/Home/TreatmentBanner/TreatmentBanner.js
@@ -3,10 +3,10 @@ import treatmentBanner from '../../../assets/images/treatment.png'
 const TreatmentBanner = () => {
     return (
         <div>
-            <div className="hero py-12">
-                <div className="hero-content flex-col lg:flex-row lg:px-20">
-                    <img src={treatmentBanner} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div className='lg:pl-14'>
+            <div className="hero py-[80px]">
+                <div className="hero-content  grid grid-cols-1 md:grid-cols-2  lg:px-10">
+                    <img src={treatmentBanner} className="h-[600px] rounded-lg shadow-2xl w-full md:w-fit" />
+                    <div className='pt-14 md:pt-0'>
                         <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                         <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                         <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
