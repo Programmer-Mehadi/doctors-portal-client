306c10ef88967f8e24afff8862f02635f49fdae6
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-12 01:56:43 +0600

    treatment banner done.

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
 import Services from '../Services/Services';
+import TreatmentBanner from '../TreatmentBanner/TreatmentBanner';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <InfoCards></InfoCards>
       <Services></Services>
+      <TreatmentBanner></TreatmentBanner>
     </div>
   );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -15,13 +15,10 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    
+
                     <Link to='/' className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
                 </div>
                 <div className="navbar-end ">
-                    <ul className="menu menu-horizontal p-0 hidden lg:flex">
-                        {menuItems}
-                    </ul>
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -30,6 +27,10 @@ const Navbar = () => {
                             {menuItems}
                         </ul>
                     </div>
+                    <ul className="menu menu-horizontal p-0 hidden lg:flex">
+                        {menuItems}
+                    </ul>
+
                 </div>
 
             </div>
